708bc1798fd96ea37b41843051cf970e1c748769
오승아 <sdesign416@naver.com>
2026-07-02 15:24:28 +0900

    로그인 API 완료

--- controller/auth.mjs
+++ controller/auth.mjs
@@ -20,12 +20,24 @@ export async function signup(req, res) {
     // 가입완료 후
     const token = await createJwtToken(user.id)
     console.log(token)
-
+    // 출력용
     res.status(201).json({token, user})
 }
 
 // 로그인
 export async function login(req, res) {
+    const {userid, password} = req.body
+    const user = await authRepository.findByUserid(userid)
+    if(user){
+        const isValidPassword = await bcrypt.compare(password, user.password)
+        if(!isValidPassword){
+            return res.status(401).json({message: "아이디 또는 비밀번호 확인!"})
+        }
+        const token = await createJwtToken(user.id)
+        res.status(200).json({token, user})
+    }else{
+
+    }
     
 }
 
