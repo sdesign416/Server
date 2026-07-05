b072cff8a8f3878140f4d87d920f28840507d485
오승아 <sdesign416@naver.com>
2026-07-05 13:47:56 +0900

    front: 기본구조 완성

--- app.mjs
+++ app.mjs
@@ -3,13 +3,27 @@ import { config } from "./config.mjs"  // config.mjs의 config 객체 가져옴
 import { connectDB } from "./db/database.mjs"
 import authRouter from "./router/auth.mjs"
 import postsRouter from "./router/posts.mjs"
+// public용
+import path from "path"
+import { fileURLToPath } from "url"
 
 const app = express()
 
+// __dirname 생성(public용)
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 // 미들웨어 등록
 app.use(express.json())
+
+// public 미들웨어 등록
+app.use(express.static(path.join(__dirname, "public")))
+
+// API
 app.use("/auth", authRouter)
 app.use("/post", postsRouter)
+
+// 404 (항상 마지막)
 app.use((req, res) => {
     res.sendStatus(404)
 }) 
